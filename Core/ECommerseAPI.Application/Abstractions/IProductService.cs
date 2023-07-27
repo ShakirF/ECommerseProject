c6a4ac096cd35e96c19023181e0b9b771eb602d9
Shakir Faracullayev <shakir.f@setclapp.com>
2023-07-27 11:48:40 +0400

    add interface

---
+++ Core/ECommerseAPI.Application/Abstractions/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using ECommerseAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerseAPI.Application.Abstractions
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+    }
+}
