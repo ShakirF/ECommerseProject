fe9bf3a35ae4d53cf15a21b037b251c272782d5d
Shakir Faracullayev <shakir.f@setclapp.com>
2023-07-31 20:01:05 +0400

    add repository

---
+++ Core/ECommerseAPI.Application/Repositories/Product/IProductWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using ECommerseAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerseAPI.Application.Repositories
+{
+    public interface IProductWriteRepository : IWriteRepository<Product>
+    {
+    }
+}
