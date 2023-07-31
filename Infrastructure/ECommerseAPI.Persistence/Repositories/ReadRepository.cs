51ec2889663f5dd1f243860c0d1e0817f6e21b97
Shakir Faracullayev <shakir.f@setclapp.com>
2023-07-31 20:01:21 +0400

    add repository

---
+++ Infrastructure/ECommerseAPI.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,37 @@
+﻿using ECommerseAPI.Application.Repositories;
+using ECommerseAPI.Domain.Entities.Common;
+using ECommerseAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerseAPI.Persistence.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+    {
+        private readonly ECommerseAPIDbContext _context;
+
+        public ReadRepository(ECommerseAPIDbContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<T> Table => _context.Set<T>();
+
+        public IQueryable<T> GetAll()
+         => Table;
+
+        public async Task<T> GetByIdAsync(string id)
+        => await Table.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method)
+        => await Table.FirstOrDefaultAsync(method);
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method)
+        => Table.Where(method);
+    }
+}
