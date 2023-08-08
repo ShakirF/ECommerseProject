07c8e716879acb54cdfdab4ae9769fadc82f9e4e
Shakir Faracullayev <shakir.f@setclapp.com>
2023-08-08 11:38:55 +0400

    push

--- Core/ECommerseAPI.Domain/Entities/Customer.cs
+++ Core/ECommerseAPI.Domain/Entities/Customer.cs
@@ -9,7 +9,7 @@ namespace ECommerseAPI.Domain.Entities
 {
     public class Customer : BaseEntity
     {
-        public string Name { get; set; }
-        public ICollection<Order> Orders { get; set; }
+        public string? Name { get; set; }
+        public ICollection<Order>? Orders { get; set; }
     }
 }
---
+++ Infrastructure/ECommerseAPI.Persistence/Migrations/20230808062227_mig_2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerseAPI.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "Products",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Products",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Orders",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Customers",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Price",
+                table: "Products",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
